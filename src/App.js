8a44ab9b530fde88bd2f7e66127a9081a2f93c34
ssolders <simon.solders@devcode.se>
2018-10-15 11:54:50 +0200

    Added some critical fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cats from './static/cats.gif'
 import './App.scss';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Nice 2018 GraphQL - Lab</h1>
+          <img alt='my-cats' src={Cats} />
 
           <a target='_blank' rel='noopener noreferrer' href="https://www.apollographql.com/docs/react/essentials/get-started.html">Readme: Setup as Apollo client</a>
         </header>
