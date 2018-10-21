fc22f09e08c62ba9970b17f1e00ce092d2444fde
ssolders <simon.solders@devcode.se>
2018-10-21 12:03:08 +0200

    The cat walked on my keyboard again

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ class App extends Component {
   componentDidMount() {
     this.fetchBooks()
   }
-  async fetchBooks(value = '') {
-    console.log(value)
-    let books = await getBooks(value, ["id", "title"])
+  async fetchBooks() {
+    let books = await getBooks(this.state.search, ["id", "title"])
     books = books.data.books
     this.setState({ books })
   }
@@ -40,7 +39,7 @@ class App extends Component {
             <input
               placeholder='Search the magic'
               onChange={e => this.handleBooks(e.target.value)}
-              onKeyUp={e => debounce(this.fetchBooks.bind(this, e.target.value), 300)()}
+              onKeyUp={e => debounce(this.fetchBooks.bind(this), 300)()}
               value={this.state.search}
               type="search"
               autoFocus={true} />
